f3974c178343aafcd7917846d79bf3b48052aff0
David Crosby <dfcrosby@gmail.com>
2016-02-26 12:59:10 -0500

    Update ex15.py
    added test for input of filename on comand line - i.e., argv includes data file name.

--- ex15.py
+++ ex15.py
@@ -1,11 +1,21 @@
 #Ex15 - Reading Files
 
 # import command line parameters
-from sys import argv
+from sys
 
 # Assign script to the name of script file
 # Assign filename to the second argument
-script, filename = argv
+# script, filename = argv
+
+print sys.argv
+
+# If argv does not include data file name (sys.argv <2), prompt for it.
+# If argv includes data file name assign it to filename.
+if len(sys.argv) < 2:
+	print "Type the filename:"
+	filename = raw_input (">")
+else:
+	script, filename = sys.argv
 
 txt = open(filename)
 
