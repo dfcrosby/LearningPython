63b35e410ad72817969b1b464eebc39a19717666
David Crosby <dfcrosby@gmail.com>
2016-02-26 13:09:19 -0500

    Update ex15b.py

--- ex15b.py
+++ ex15b.py
@@ -1,11 +1,14 @@
 # Example 15
 #
 
+# import system command line arguments.
 from sys import argv
 
+# Assign script to be the command line variables.
 script = argv
 
-print "The script is call: ", script
+print "The script[0] is called: ", script[0]
+print "the script[1] is called: ", script[1]
 print "The argv is: ", argv
 
 print "done."
